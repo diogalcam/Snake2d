436f6022e4f8281c55c128519b2c3f135f2e845d
adriperezguti <us55AS4M>
2020-04-22 12:33:58 +0200

    musica medio lista

--- app/src/main/java/com/example/snake2d/MenuActivity.java
+++ app/src/main/java/com/example/snake2d/MenuActivity.java
@@ -1,37 +1,26 @@
 package com.example.snake2d;
 
 import android.content.Intent;
-import android.content.res.ColorStateList;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.media.MediaPlayer;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.media.AudioManager;
 
+import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
-import com.google.android.gms.tasks.Continuation;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.mongodb.lang.NonNull;
 import com.mongodb.stitch.android.core.Stitch;
 import com.mongodb.stitch.android.core.StitchAppClient;
-import com.mongodb.stitch.android.core.auth.StitchUser;
-import com.mongodb.stitch.android.services.mongodb.remote.RemoteFindIterable;
-import com.mongodb.stitch.android.services.mongodb.remote.RemoteMongoClient;
 import com.mongodb.stitch.android.services.mongodb.remote.RemoteMongoCollection;
-import com.mongodb.stitch.core.auth.providers.anonymous.AnonymousCredential;
-import com.mongodb.stitch.core.services.mongodb.remote.RemoteDeleteResult;
-import com.mongodb.stitch.core.services.mongodb.remote.RemoteInsertOneResult;
-import com.mongodb.stitch.core.services.mongodb.remote.RemoteUpdateOptions;
-import com.mongodb.stitch.core.services.mongodb.remote.RemoteUpdateResult;
 
 import org.bson.Document;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -40,6 +29,18 @@ public class MenuActivity extends AppCompatActivity {
     private Button button1;
     private TextView textView2;
     private Button button2;
+
+    private ImageButton muteButton;
+    private String muteText;
+
+    static SoundPool soundPool;
+    int music = -1;
+    int i = 0;
+
+
+
+
+
     public RemoteMongoCollection<Document> datos;
     ResourceBundle resources;
     StitchAppClient stitchClient;
@@ -55,10 +56,14 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
+        loadSound();
+
+
+
         //el texto de SNAKE lo he puesto un poco cutre LUL pero se le podría mirar alguna manera de mejorarlo
-        textView2 = (TextView) findViewById(R.id.textView2);
-        textView2.setTextSize(30);
-        textView2.setTextColor(Color.WHITE);
+        //textView2 = (TextView) findViewById(R.id.textView2);
+        //textView2.setTextSize(30);
+        //textView2.setTextColor(Color.WHITE);
 
         //el botón de JUGAR(button1) . El otro botón de MARCADORES todavía no hace nada simplemente se muestra
         button1 = (Button) findViewById(R.id.button1);
@@ -77,6 +82,15 @@ public class MenuActivity extends AppCompatActivity {
             }
         });
 
+
+        muteButton = (ImageButton) findViewById(R.id.imageButton2);
+        muteButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                changeMute();
+            }
+        });
+
+
         StitchAppClient client = Stitch.initializeDefaultAppClient("snake2d-deosi");
 
 
@@ -86,11 +100,55 @@ public class MenuActivity extends AppCompatActivity {
     public void openActivity2(){
         Intent intent = new Intent(this,MainActivity.class);
         startActivity(intent);
+        //Si le damos a jugar se para la musica del menu
+        soundPool.stop(1);
+
     }
 
     public void openActivity3(){
         Intent intent = new Intent(this,Marcador.class);
         startActivity(intent);
+        //Podria ponerse aqui tambien para la musica o dejarla nose que es mejor
+    }
+
+    //Funcion para manejar el MUTE
+    public void changeMute() {
+        muteText = muteButton.getTransitionName();
+        if (muteText == "muteoff") {
+            muteButton.setTransitionName("muteon");
+            muteButton.setImageResource(R.drawable.muteon);
+            soundPool.pause(1);
+
+
+        } else {
+            muteButton.setTransitionName("muteoff");
+            muteButton.setImageResource(R.drawable.muteoff);
+            //la primera vez hará play, y la segunda hará reanudar
+            if(i==0){
+                soundPool.play(music, 1, 1, 0, -1, 1);
+                i = i + 1;
+            }else{
+                soundPool.resume(1);
+            }
+
+        }
+    }
+    public void loadSound(){
+        soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+        try {
+            //requerido para abrir los sonidos
+            AssetManager assetManager = getAssets();
+            AssetFileDescriptor descriptor;
+
+
+            descriptor = assetManager.openFd("music.ogg");
+            music = soundPool.load(descriptor, 0);
+
+        } catch (IOException e) {
+
+            Log.e("error", "fallo al carga los sonidos");
+
+        }
     }
 
 }
